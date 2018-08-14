2a07308bbbb19fcf9b8a3609448646720018a774
arcyro <arek@arcyro.pl>
2018-08-14 15:04:48 +0200

    develop branch

---
+++ src/main/java/exam/some_Issue.java
@@ -0,0 +1,18 @@
+package exam;
+
+public class some_Issue {
+    public static void main(String[] args) {
+
+    }
+
+    private int some_method(String  aaaa){
+        System.out.println("ssss");
+        System.out.println("ssss");
+        System.out.println("ssss");
+        System.out.println("ssss");
+        int a_b =12, c_d=22;
+
+        int f_g = a_b+ c_d;
+        return f_g;
+    }
+}
