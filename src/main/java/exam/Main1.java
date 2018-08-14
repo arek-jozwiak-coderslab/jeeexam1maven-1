93395a5d143db8c6413eaa757263454c35900c0e
arcyro <arek@arcyro.pl>
2018-08-14 13:00:12 +0200

    sample solution

--- src/main/java/exam/Main1.java
+++ src/main/java/exam/Main1.java
@@ -5,7 +5,8 @@ public class Main1 {
 	public static void main(String[] args){
 		
 		
-		System.out.println("sizes : " + 5 + " and " + 4 + " rectangle is: " + rectangle(5, 4));
+
+			System.out.println("sizes : " + 5 + " and " + 4 + " rectangle is: " + rectangle(5, 4));
 		
 		
 		
@@ -15,6 +16,7 @@ public class Main1 {
 	
 	static int rectangle(int a, int b){
 	
+
 		return a * b+1;
 		
 	}
