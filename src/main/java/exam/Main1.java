607973c51ff0ef5cbc7e50ef0714efc620cf3312
arcyro <arek@arcyro.pl>
2018-08-14 14:31:58 +0200

    develop branch

--- src/main/java/exam/Main1.java
+++ src/main/java/exam/Main1.java
@@ -4,7 +4,7 @@ public class Main1 {
 	
 	public static void main(String[] args){
 
-			System.out.println(" develop sizes : " + 5 + " and " + 4 + " rectangle is: " + rectangle(5, 4));
+			System.out.println(" sizes : " + 5 + " and " + 4 + " rectangle is: " + rectangle(5, 4));
 		
 	}
 	
